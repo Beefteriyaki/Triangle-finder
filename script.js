c8f4cad48ea9166e92eac528c91cc2f87a33c73e
Alex David <ad26creations@gmail.com>
2022-06-04 11:47:07 +0530

    Update script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+
+const button = document.querySelector(".finder");
+
+button.addEventListener("click",function(){
+    let text = document.querySelector(".output").textContent;
+    
+    const a = document.querySelector(".length1").value;
+    const b = document.querySelector(".length2").value;
+    const c = document.querySelector(".length3").value;
+
+    if(a+b>c && b+c>a && a+c>b){
+        text = "The given sides can form a triangle"
+    }
+});
