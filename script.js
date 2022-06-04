6fc84de1167367cccd1190609e8aefebe6472541
Alex David <ad26creations@gmail.com>
2022-06-04 22:04:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,14 +2,24 @@
 const button = document.querySelector(".finder");
 
 button.addEventListener("click",function(){
-    let text = document.querySelector(".output").textContent;
     
-    const a = document.querySelector(".length1").value;
-    const b = document.querySelector(".length2").value;
-    const c = document.querySelector(".length3").value;
+    const a = Number(document.querySelector(".length1").value);
+    const b = Number(document.querySelector(".length2").value);
+    const c = Number(document.querySelector(".length3").value);
 
+    console.log(a, b, c);
+    
+    
+    if( a+b > c && b+c > a && a+c > b){
+    
+        document.querySelector(".output").value = "These sides can form a triangle";
+        console.log(document.querySelector(".output").value);
+    
+    }
 
-    if(a+b>c && b+c>a && a+c>b){
-        text = "The given sides can form a triangle"
+    else{
+        
+        document.querySelector(".output").value = "These sides cannot form a triangle";
+        console.log(document.querySelector(".output").value);
     }
 });
